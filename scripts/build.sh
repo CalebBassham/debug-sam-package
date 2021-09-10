05c94fb29da02e675360e024d85ac9fe977834c6
Caleb Bassham <caleb.bassham@gmail.com>
2021-09-10 13:34:30 -0500

    move sam files to `deployments` directory and add flags to sam-cli calls to indicate where the files are located.
    Function does not work: Internal server error
    2021-09-10T13:33:35.241-05:00	START RequestId: bfb28beb-3e08-4c01-9f5b-a1f56d254f01 Version: $LATEST
    2021-09-10T13:33:35.249-05:00	END RequestId: bfb28beb-3e08-4c01-9f5b-a1f56d254f01
    2021-09-10T13:33:35.249-05:00	REPORT RequestId: bfb28beb-3e08-4c01-9f5b-a1f56d254f01 Duration: 3.61 ms Billed Duration: 4 ms Memory Size: 128 MB Max Memory Used: 6 MB XRAY TraceId: 1-613ba4ff-619a1c7d2db4e68a6afa87b8 SegmentId: 7bf02fe0422b87c9 Sampled: true
    2021-09-10T13:33:35.249-05:00	RequestId: bfb28beb-3e08-4c01-9f5b-a1f56d254f01 Error: Couldn't find valid bootstrap(s): [/var/task/bootstrap /opt/bootstrap] Runtime.InvalidEntrypoint
    
    Package contains entire repo.
    sam-package
    ├── deployments
    │   ├── samconfig.toml
    │   └── template.yaml
    ├── go.mod
    ├── hello-world
    │   ├── go.mod
    │   ├── go.sum
    │   ├── main.go
    │   └── main_test.go
    ├── Makefile
    ├── README.md
    └── scripts
        ├── builds
        │   └── hello-world.sh
        ├── build.sh
        ├── clean.sh
        └── deploy.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
 cd `dirname $0`/..
 
-sam build
+DEPLOYMENTS_DIR=`pwd`/deployments
+sam build --template $DEPLOYMENTS_DIR/template.yaml --config-file $DEPLOYMENTS_DIR/samconfig.toml
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 cd `dirname $0`/.. # Move to project root
 
-AWS_PROFILE=unitprints sam deploy
+DEPLOYMENTS_DIR=`pwd`/deployments
+AWS_PROFILE=unitprints sam deploy --template $DEPLOYMENTS_DIR/template.yaml --config-file $DEPLOYMENTS_DIR/samconfig.toml
